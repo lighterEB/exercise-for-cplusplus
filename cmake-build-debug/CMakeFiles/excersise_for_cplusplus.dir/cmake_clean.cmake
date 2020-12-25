fcb82b3cd13e81cd4bd58e836e1819c42507f305
lighterEB <hellohus726@126.com>
2020-12-25 15:48:07 +0800

    day 01

---
+++ cmake-build-debug/CMakeFiles/excersise_for_cplusplus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/excersise_for_cplusplus.dir/day_01/maopao.cpp.obj"
+  "excersise_for_cplusplus.exe"
+  "excersise_for_cplusplus.exe.manifest"
+  "excersise_for_cplusplus.pdb"
+  "libexcersise_for_cplusplus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/excersise_for_cplusplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- day 01/maopao.cpp
+++ day 01/maopao.cpp
@@ -7,12 +7,30 @@ using namespace std;
 
 int main()
 {
-    int arry[10] = {20, 5, 6, 8, 13, 57, 12, 3, 2, 7};
+    int arry[9] = { 3,0,5,7,8,4,10,23,16};
     cout << "ÅÅĞòÇ°£º" << endl;
-
     for (int i : arry)
     {
         cout << i << endl;
     }
+    // Ã°ÅİÅÅĞò
+    for (int i = 0; i < 9 - 1; i++)
+    {
+        for(int j = 0; j< 9 - i - 1; j++)
+        {
+            if (arry[j + 1] < arry[j])
+            {
+                int temp = arry[j + 1];
+                arry[j + 1] = arry[j];
+                arry[j] = temp;
 
+            }
+        }
+    }
+    cout << "ÅÅĞòºó£º" << endl;
+    for (int i : arry)
+    {
+        cout << i << endl;
+    }
+    return 0;
 }
