a39f35aab7649f5eafc6963e74e552a3b46f4645
lighterEB <hellohus726@126.com>
2020-12-28 22:34:08 +0800

    day 02

---
+++ day 02/calcarea.cpp
@@ -0,0 +1,28 @@
+﻿//
+// Created by tosixJuly on 2020/12/28.
+//
+
+#include <iostream>
+using namespace std;
+
+void CalcArea(const double* const pPi, const double* const pRadius, double* const pArea)
+{
+    if(pPi && pRadius && pArea)
+    {
+        *pArea = (*pPi) * (*pRadius) * (*pRadius);
+    }
+}
+
+int main()
+{
+    double const Pi = 3.1415926;
+    double Radius = 0;
+    cout << "Enter radius: " << endl;
+    cin >> Radius;
+    double Area = 0;
+    CalcArea(&Pi, &Radius, &Area);
+
+    cout << "Area is: " << Area << endl;
+
+    return 0;
+}
