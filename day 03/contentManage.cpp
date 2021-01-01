6c8349e01a762929ad89a9ef4edde20b8cba563c
lighterEB <hellohus726@126.com>
2021-01-01 16:54:37 +0800

    day 03

--- day 03/contentManage.cpp
+++ day 03/contentManage.cpp
@@ -13,15 +13,40 @@ using namespace std;
 // 菜单显示
 void showMenue()
 {
+    cout << "*************************" << endl;
     cout << "****** 1、添加联系人 ******" << endl;
     cout << "****** 2、显示联系人 ******" << endl;
     cout << "****** 3、删除联系人 ******" << endl;
     cout << "****** 4、查找联系人 ******" << endl;
     cout << "****** 5、清空联系人 ******" << endl;
     cout << "****** 0、退出通讯录 ******" << endl;
+    cout << "*************************" << endl;
 }
 int main()
 {
-    showMenue();
+    int select = 0; // 创建选择变量
+    // 菜单调用
+    while (true)
+    {
+        showMenue();
+        cin >> select;
+        switch (select) {
+            case 1:     // 添加联系人
+                break;
+            case 2:     // 显示联系人
+                break;
+            case 3:     // 删除联系人
+                break;
+            case 4:     // 查找联系人
+                break;
+            case 5:     // 修改联系人
+                break;
+            case 6:     // 清空联系人
+                break;
+            case 0:     // 退出通讯录
+                cout << "欢迎下次使用！"<<endl;
+                return 0;
+        }
+    }
     return 0;
 }
