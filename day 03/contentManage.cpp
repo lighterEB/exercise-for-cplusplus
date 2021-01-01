8238c57e3d51c8d03df9f496e6ab5ae998deca85
lighterEB <hellohus726@126.com>
2021-01-01 17:20:56 +0800

    day 03

--- day 03/contentManage.cpp
+++ day 03/contentManage.cpp
@@ -2,7 +2,7 @@
 // Created by tosixJuly on 2021/1/1.
 //
 /*
-	通讯录管理系统
+    通讯录管理系统
 */
 
 #include <iostream>
@@ -13,14 +13,14 @@ using namespace std;
 // 菜单显示
 void showMenue()
 {
-    cout << "*************************" << endl;
+    cout << "***************************" << endl;
     cout << "****** 1、添加联系人 ******" << endl;
     cout << "****** 2、显示联系人 ******" << endl;
     cout << "****** 3、删除联系人 ******" << endl;
     cout << "****** 4、查找联系人 ******" << endl;
     cout << "****** 5、清空联系人 ******" << endl;
     cout << "****** 0、退出通讯录 ******" << endl;
-    cout << "*************************" << endl;
+    cout << "***************************" << endl;
 }
 
 // 联系人结构体
@@ -38,8 +38,38 @@ struct ContactManage
     struct Person personArray[MAX];
     int mSize{};  // 通讯录人数数量
 };
+
+// 添加联系人函数
+void addPerson(struct ContactManage* cm)
+{
+    // 判断通讯录是否满，满了就退出
+    if (cm->mSize == MAX)
+    {
+        cout << "通讯录已满，无法添加！" << endl;
+        return;
+    } else
+    {
+        // 添加具体联系人
+
+        // 姓名
+        string name;
+        cout << "请输入姓名：" << endl;
+        cin >> name;
+        cm->personArray[cm->mSize].name = name;
+        // 性别
+
+        // 年龄
+
+        // 电话
+
+        // 住址
+    }
+}
+
 int main()
 {
+    ContactManage cm;
+    cm.mSize = 0;
     int select = 0; // 创建选择变量
     // 菜单调用
     while (true)
@@ -48,6 +78,7 @@ int main()
         cin >> select;
         switch (select) {
             case 1:     // 添加联系人
+                addPerson(&cm);     //利用地址传递修改实参
                 break;
             case 2:     // 显示联系人
                 break;
@@ -60,7 +91,7 @@ int main()
             case 6:     // 清空联系人
                 break;
             case 0:     // 退出通讯录
-                cout << "欢迎下次使用！"<<endl;
+                cout << "欢迎下次使用！" << endl;
                 return 0;
         }
     }
