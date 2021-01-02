bdc5aa7ab26cc117979cb75c05f189d79d2e6625
lighterEB <hellohus726@126.com>
2021-01-02 20:38:40 +0800

    day 03

--- day 03/contentManage.cpp
+++ day 03/contentManage.cpp
@@ -13,12 +13,14 @@ using namespace std;
 // 菜单显示
 void showMenue()
 {
+    system("cls");
     cout << "***************************" << endl;
     cout << "****** 1、添加联系人 ******" << endl;
     cout << "****** 2、显示联系人 ******" << endl;
     cout << "****** 3、删除联系人 ******" << endl;
     cout << "****** 4、查找联系人 ******" << endl;
-    cout << "****** 5、清空联系人 ******" << endl;
+    cout << "****** 5、修改联系人 ******" << endl;
+    cout << "****** 6、清空联系人 ******" << endl;
     cout << "****** 0、退出通讯录 ******" << endl;
     cout << "***************************" << endl;
 }
@@ -54,6 +56,7 @@ void addPerson(struct ContactManage* cm)
 
         // 姓名
         string name;
+        system("cls");
         cout << "请输入姓名：" << endl;
         cin >> name;
         cm->personArray[cm->mSize].name = name;
@@ -94,6 +97,7 @@ void addPerson(struct ContactManage* cm)
         // 更新通讯录
         cm->mSize++;
         cout << "添加成功！" << endl;
+        system("pause");
     }
 }
 
@@ -102,25 +106,28 @@ void showPerson(ContactManage* cm)
 {
     // 判断通讯录记录是否为空，如果为空提示记录为空
     // 如果不为0，显示记录中的联系人
-    if(cm->mSize == 0)
+    system("cls");
+    if (cm->mSize == 0)
     {
         cout << "通讯录为空" << endl;
+        system("pause");
     }
     else
     {
         cout << "联系人信息：" << endl;
-        for(int i = 0; i < cm->mSize; i++)
+        for (int i = 0; i < cm->mSize; i++)
         {
             cout << "姓名：" << cm->personArray[i].name << "\t"
-                    << "年龄：" << cm->personArray[i].age << "\t"
-                    << "性别：" << cm->personArray[i].sex << "\t"
-                    << "电话：" << cm->personArray[i].phoneNum << "\t"
-                    << "住址：" << cm->personArray[i].address << "\t" << endl;
+                 << "年龄：" << cm->personArray[i].age << "\t"
+                 << "性别：" << (cm->personArray[i].sex == 1 ? "男" : "女") << "\t"
+                 << "电话：" << cm->personArray[i].phoneNum << "\t"
+                 << "住址：" << cm->personArray[i].address << "\t" << endl;
         }
+        system("pause");
     }
 }
 
-// 查找联系人
+// 判断联系人是否存在
 int isExist(ContactManage* cm, const string& name)
 {
     for (int i = 0; i < cm->mSize; i++)
@@ -136,22 +143,77 @@ int isExist(ContactManage* cm, const string& name)
 // 删除联系人函数
 void deletePerson(ContactManage* cm)
 {
+    system("cls");
     cout << "请输入要删除的联系人姓名：" << endl;
     string name;
     cin >> name;
     int ret = isExist(cm, name);
-    if(ret == -1)
-    {
-        cout << "查无此人" << endl;
-    }
-    else
+    if (ret != -1)
     {
         for (int i = ret; i < cm->mSize; i++)
         {
-            cm->personArray[i] = cm->personArray[i+1];
+            cm->personArray[i] = cm->personArray[i + 1];
         }
         cm->mSize--;
+        cout << "删除成功！" << endl;
     }
+    else
+    {
+        cout << "查无此人" << endl;
+    }
+    system("pause");
+    system("cls");
+}
+
+// 查找联系人
+void findPerson(ContactManage* cm)
+{
+    system("cls");
+    int ret;
+    string name;
+    cout << "请输入您要查找的联系人：" << endl;
+    cin >> name;
+    ret = isExist(cm,name);
+    if (ret != -1)
+    {
+        cout << "姓名：" << cm->personArray[ret].name << "\t"
+             << "年龄：" << cm->personArray[ret].age << "\t"
+             << "性别：" << (cm->personArray[ret].sex == 1 ? "男" : "女") << "\t"
+             << "电话：" << cm->personArray[ret].phoneNum << "\t"
+             << "住址：" << cm->personArray[ret].address << "\t" << endl;
+    }
+    else
+    {
+        cout << "查无此人！" << endl;
+    }
+    system("pause");
+}
+
+// 修改联系人
+void modifyPerson(ContactManage* cm)
+{
+    system("cls");
+    string name;
+    int ret;
+    cout << "请输入要修改的联系人：" << endl;
+    cin >> name;
+    ret = isExist(cm, name);
+    if (ret != -1)
+    {
+        cout << "请输入要修改的信息：" << endl;
+        cout << "姓名： " << endl;
+        cin >> name;
+
+    }
+}
+
+// 清空联系人
+void clearPerson(ContactManage* cm)
+{
+    cm->mSize = 0;
+    system("cls");
+    cout << "通讯录已清空！" << endl;
+    system("pause");
 }
 
 int main()
@@ -175,10 +237,12 @@ int main()
                 deletePerson(&cm);
                 break;
             case 4:     // 查找联系人
+                findPerson(&cm);
                 break;
             case 5:     // 修改联系人
                 break;
             case 6:     // 清空联系人
+                clearPerson(&cm);
                 break;
             case 0:     // 退出通讯录
                 cout << "欢迎下次使用！" << endl;
