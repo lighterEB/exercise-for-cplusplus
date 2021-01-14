cbccfbd4eb1aa18de0a7540ea8a669b63833cbf6
lighterEB <hellohus726@126.com>
2021-01-15 00:24:40 +0800

    day 07

---
+++ day 06/newThrow.cpp
@@ -0,0 +1,24 @@
+﻿//
+// Created by tosixJuly on 2021/1/15.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Request lots of memory space, use nothrow version
+    int* pAge = new(nothrow) int [0x1fffffff];
+
+    if(pAge)    // check pAge != NULL
+    {
+        // Use the allocated memory
+        delete[] pAge;
+    }
+    else
+    {
+        cout << "Memory allocated failed. Ending program" << endl;
+    }
+
+    return 0;
+}
