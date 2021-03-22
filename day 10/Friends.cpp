bab9746e52a42ac245fbe8b68b9d647583316725
lighterEB <hellohus726@126.com>
2021-03-22 11:21:40 +0800

    day 10

--- day 10/Friends.cpp
+++ day 10/Friends.cpp
@@ -2,3 +2,32 @@
 // Created by tosixJuly on 2021/3/22.
 //
 
+#include <iostream>
+using namespace std;
+
+class Building
+{
+
+public:
+    string m_SittingRoom;
+
+    Building()
+    {
+        m_SittingRoom = "Sitting Room";
+        m_BedRoom = "Bed Room";
+    }
+
+private:
+    string m_BedRoom;
+};
+
+void goodGay(Building* building)
+{
+    cout << "好基友的全局函数 正在访问 ：" << building->m_SittingRoom << endl;
+}
+
+void test01()
+{
+    Building building;
+    goodGay(&building);
+}
